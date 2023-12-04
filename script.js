ea29b776aaa05b4612d4830f4747590628f2ab91
udaykiranchimpiri <147032952+udaykiranchimpiri@users.noreply.github.com>
2023-12-04 01:27:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-document.getElementById('contactForm').addEventListener('submit', function(event){
-    event.preventDefault();
-    alert('Form submitted! We will get back to you soon.');
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('contactForm');
+    if (form) {
+        form.addEventListener('submit', function(event){
+            event.preventDefault();
+            alert('Form submitted! We will get back to you soon.');
+        });
+    }
 });
